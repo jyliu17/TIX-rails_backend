b375805e79c38ab1b8d5170e19297d650d0648a0
jyliu17 <jyliu17@gmail.com>
2021-05-28 16:20:31 -0400

    initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #testing testing
 
-# User.create(
-#   email: "Ian@flatiron.com",
-#   password: "123",
-#   first_name: "Ian",
-#   last_name: "McKalan",
-#   is_agent: True,
-#   is_admin: True
-# )
+User.create(
+  email: "Ian@flatiron.com",
+  password: "123",
+  firstname: "Ian",
+  lastname: "McKalan",
+  is_agent: TRUE,
+  is_admin: TRUE
+)
+
+Ticket.create(
+    name: "IT Issue",
+    description:"Rails not working",
+    assigned_to: "Ian",
+    category: "IT",
+    sub_category: "IT",
+    priority: "1",
+)
+
+UserTicket.create( user_id: 1, ticket_id: 1)
